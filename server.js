6062bd43d8a2ebb9ef83399092ed80b00058bad5
shazzmarquee <30423936+shazzmarquee@users.noreply.github.com>
2019-06-13 23:53:29 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,89 +1,117 @@
 
-
+var fs = require('fs');
 var path = require('path');
 var express = require('express');
-var exphbs = require('express-handlebars')
+var exphbs = require('express-handlebars');
+var bodyParser = require('body-parser');
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 var port = process.env.PORT || 3000;
 
+var mongoHost = process.env.MONGO_HOST;
+var mongoPort = 27017;
+var mongoUser = process.env.MONGO_USER;
+var mongoPassword = process.env.MONGO_PASSWORD;
+var mongoDBName = "surveyDB";
 
-var surveyData = require('./surveyData');
-console.log("== surveyData", surveyData);
+var mongoUrl = 'mongodb://localhost:27017/';
+var db = null;
 
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+app.use(bodyParser.json());
+
 app.use(express.static('public'));
 
-
-
-
 app.get('/', function(req,res) {
-	/*var person = req.params.person.toLowerCase();
-  	if (surveyData[person]) {
-    	res.status(200).render('surveyPage', surveyData[person]);
-    // res.status(200).sendFile(
-    //   __dirname + '/public/people/' + person + '.html'
-    // );
-  	} else {
-    	next();
-  	}*/
-
-	res.status(200).render('homePage',surveyData);
+    var collection = db.collection('test');
+    collection.find({}).toArray(function (err, serv){
+    if (err){
+        res.status(500).send({
+            error: "Error fetching survey from DB"
+        });
+    } else{
+        console.log("== surveys:", serv[0]);
+        res.status(200).render('homePage', serv[0]);
+    }   
+    });
 });
 
-app.get('/:num', function(req,res,next) {
-  /*var person = req.params.person.toLowerCase();
-    if (surveyData[person]) {
-      res.status(200).render('surveyPage', surveyData[person]);
-    // res.status(200).sendFile(
-    //   __dirname + '/public/people/' + person + '.html'
-    // );
-    } else {
-      next();
-    }*/
+app.get('/:num', function(req,res) {
 
-    console.log("== Data Length: ",surveyData["surveys"].length);
     var num = req.params.num.toLowerCase();
     num = parseInt(num,10);
     num--;
-
-    if(num >= 0 && num < surveyData["surveys"].length){
-
-      res.status(200).render('surveyPage',surveyData["surveys"][num]);
-    }
-    else{
-      next();
-    }
+    var collection = db.collection('test');
+    collection.find({}).toArray(function (err, serv){
+    if (err){
+        res.status(500).send({
+            error: "Error fetching survey from DB"
+        });
+    } else{
+        console.log("== serv[0]{}:", serv[0].surveys[num]);
+        res.status(200).render('surveyPage', serv[0].surveys[num]);
+    }   
+    });
 });
-
 app.get('/results/:num', function(req,res,next) {
-  /*var person = req.params.person.toLowerCase();
-    if (surveyData[person]) {
-      res.status(200).render('surveyPage', surveyData[person]);
-    // res.status(200).sendFile(
-    //   __dirname + '/public/people/' + person + '.html'
-    // );
-    } else {
-      next();
-    }*/
 
-    console.log("== Data Length: ",surveyData["surveys"].length);
     var num = req.params.num.toLowerCase();
     num = parseInt(num,10);
     num--;
 
-    if(num >= 0 && num < surveyData["surveys"].length){
+    var collection = db.collection('test');
+    collection.find({}).toArray(function (err, serv){
+     console.log("== Data Length: ",serv[0].surveys.length);
+    if(num >= 0 && num < serv[0].surveys.length)
+    {
+                console.log("== serv[0]{}:", serv[0].surveys[num]);
+        res.status(200).render('resultPage', serv[0].surveys[num]);
 
-      res.status(200).render('resultPage',surveyData["surveys"][num]);
+    } 
+    else
+    {
+                res.status(404).render('404');
+    }   
+    }); 
 
-    }
-    else{
-      next();
-    }
 });
-
+app.post('/addSurvey', function (req, res) 
+{
+if (req.body && req.body.title && req.body.questions)
+    {
+    var collection = db.collection('test');
+    collection.find({}).toArray(function (err, serv)
+        {
+        var leg = serv[0].surveys.length;
+        var bURL = './' + leg;
+        var resURL = './results' + leg;
+        var serv = 
+        {
+        title: req.body.title,
+        url: bURL,
+        resultURL: resURL,         
+        questions: req.body.questions
+        };
+        collection.updateOne({},{ $push: {surveys: serv}},function (err, result) {
+        if (err) {
+          res.status(500).send({
+            error: "Error inserting twit into DB"
+          });
+        } else {
+          console.log("== update result:", result);
+          if (result.matchedCount > 0) {
+            res.status(200).send("Success");
+          } else {
+            next();
+          }
+        }
+      });
+  } 
+)}
+});
 
 
 app.get('*', function (req, res) {
@@ -91,12 +119,12 @@ app.get('*', function (req, res) {
 });
 
 
-/*
-app.get('*', function (req, res) {
-  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
-});
-*/
-
-app.listen(port, function () {
-  console.log("== Server is listening on port", port);
+MongoClient.connect(mongoUrl, { useNewUrlParser: true },function (err, client) {
+  if (err) {
+    throw err;
+  }
+  db = client.db(mongoDBName);
+  app.listen(port, function () {
+    console.log("== Server listening on port", port);
+  });
 });
